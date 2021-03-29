10acbdadef7678139ce434b196221114f6cebd2a
giandonatoFarina <giandonato.farina@gmail.com>
2021-03-29 10:04:40 +0200

    Project skeleton

---
+++ src/main/kotlin/it/polito/wa2group8/wallet/WalletApplication.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2group8.wallet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WalletApplication
+
+fun main(args: Array<String>) {
+    runApplication<WalletApplication>(*args)
+}
