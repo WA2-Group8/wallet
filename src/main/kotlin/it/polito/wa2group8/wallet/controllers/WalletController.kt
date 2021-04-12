ef3a7a95156bb0ad6406faa350bc24eacb63c2ec
fabio_salinardi <s269426@studenti.polito.it>
2021-04-12 14:34:15 +0200

    Added Post method in WalletController for create a new wallet

--- src/main/kotlin/it/polito/wa2group8/wallet/controllers/WalletController.kt
+++ src/main/kotlin/it/polito/wa2group8/wallet/controllers/WalletController.kt
@@ -1,6 +1,7 @@
 package it.polito.wa2group8.wallet.controllers
 
 import it.polito.wa2group8.wallet.dto.TransactionDTO
+import it.polito.wa2group8.wallet.dto.WalletDTO
 import it.polito.wa2group8.wallet.services.WalletService
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
@@ -8,6 +9,15 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class WalletController(val walletService: WalletService)
 {
+    @PostMapping(value = ["/wallet"])
+    @ResponseBody
+    fun createWallet(
+        @RequestBody customerId: Long
+    ): WalletDTO?
+    {
+        return walletService.createWallet(customerId)
+    }
+
     @GetMapping(value = ["/wallets/{id}/transactions"], produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
     fun getWalletTransactionsById(@PathVariable("id") wallet_id: Long): List<TransactionDTO>
