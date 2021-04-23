0392e47ca071bd89038759dffb6f0e77df985a56
Riccardo Tedesco <42493739+ricky96highlander@users.noreply.github.com>
2021-04-23 15:59:19 +0200

    Added passwordEncoder bean

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,11 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.hibernate.validator:hibernate-validator")
     implementation("javax.validation:validation-api")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    implementation("org.springframework.boot:spring-boot-starter-security")
 }
 
 tasks.withType<KotlinCompile> {
